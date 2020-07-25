844fd8c0b701329780c823bbbcc6645aa691ba34
FabinhoEdinei <59501299+FabinhoEdinei@users.noreply.github.com>
2020-07-25 08:33:38 -0300

    Saldacao Hello word!

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var http = require("http");
 //create a server object:
 http
   .createServer(function(req, res) {
-    res.write("./routes"); //write a response to the client
+    res.write("Hola mundo"); //write a response to the client
     res.end(); //end the response
   })
   .listen(8080); //the server object listens on port 8080
